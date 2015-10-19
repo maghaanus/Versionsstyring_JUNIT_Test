0979f6a9f99dd4686d3f135c132fd26008f9b3a1
magnusmisbat <magnusmisbat@10.16.101.140>
2015-10-19 16:48:41 +0200

    Tilføjede testBilletSalg metode

--- src/automat/TestCases.java
+++ src/automat/TestCases.java
@@ -7,20 +7,32 @@ import org.junit.Test;
 public class TestCases {
 
 	Billetautomat automat = new Billetautomat();
-	
+
 	@Test
 	public void testAfMontørkoderSetBilletpris() {
-		 
+
 		int retur;
-		
-		automat.montørLogin("6789");	
+
+		automat.montørLogin("6789");
 		automat.setBilletpris(20);
 		retur = automat.getBilletpris();
-		
-		assertEquals(10, retur);
-		
-	}
-	
 
+		assertEquals(10, retur);
+
+	}
+
+	@Test
+	public void testBilletSalg() {
+
+		int retur;
+
+		automat.indsætPenge(0);
+		automat.udskrivBillet();
+		retur = automat.returpenge();
+
+		assertEquals(0, retur);
+
+		int antalFejl = 0;
+	}
 
 }
